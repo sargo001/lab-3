52b27dc5a5e15087f0b6cced79fe89bfd8bada77
sargo001 <61858818+sargo001@users.noreply.github.com>
2021-06-25 13:10:30 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace laba_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа 3.");
+            int v = 32;
+            Console.WriteLine( + ((v % 10) + 1) + " Вариант");
+
+            Console.Write("Введите количество элементов:");
+            var len = int.Parse(Console.ReadLine());
+
+            var array = new int[len];
+
+            int index = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+
+                if (max <= array[i])
+                {
+                    max = array[i];
+                    index = i;
+                }
+            Console.WriteLine("Индекс первого максимального элемента " + ": " + +index);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
